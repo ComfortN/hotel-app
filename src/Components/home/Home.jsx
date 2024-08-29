fdb6ba319e373f61ac04e8812966edc8e67fe714
Comfort Ngwenya <>
2024-08-29 16:38:36 +0200

    landing page design why chooce us section

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Home.css';
 import Banner from '../baner/Banner';
+import LandingAbout from '../Landing-About/LandingAbout';
+import DiscoverAccommodation from '../discoverAccommodation/DiscoverAccommodation';
+import WhyChooseUs from '../WhyChooseUs/WhyChooseUs';
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <LandingAbout />
+      <DiscoverAccommodation />
+      <WhyChooseUs />
     </div>
   )
 }
