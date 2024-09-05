da72752d52978d0ce0b1f3d9bc7465b2b55e9d6a
Comfort Ngwenya <>
2024-09-06 00:54:59 +0200

    adding the booking slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import accommodationReducer from './accommodationSlice';
 import favoritesSlice from './favoritesSlice';
+import cartReducer from './cartSlice';
+import bookingReducer from './bookingSlice'
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         accommodations: accommodationReducer,
         favorites: favoritesSlice,
+        cart: cartReducer,
+        booking: bookingReducer,
     },
 });
 
